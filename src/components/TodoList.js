0786cef6da0f5a51e7ac9a4d3d956425bb013cf7
joshua <wujoshua56@gmail.com>
2020-10-29 01:55:10 -0400

    coding

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,10 +3,18 @@ import TodoForm from './TodoForm';
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+    const newTodos = [todo, ...todos];
+    setTodos(newTodos);
+  };
   return (
     <div>
       <h1>Plan's for today</h1>
-      <TodoForm />
+      <TodoForm onSubmit={addTodo} />
     </div>
   );
 }
